5490d3a237dbb96c831af2d7fee56c729f8bdab0
Ikimasuu <kharl.patrick.cedeno420@gmail.com>
2026-05-12 10:12:17 +0800

    feat: implemented PointsBasedGrading

--- src/main/java/com/academiq/grading/PointsBasedGrading.java
+++ src/main/java/com/academiq/grading/PointsBasedGrading.java
@@ -14,19 +14,74 @@ public class PointsBasedGrading implements GradingPolicy {
 
     @Override
     public double computeFinalGrade(List<Assessment> assessments) {
-        // TODO
-        return 0.0;
+        if (assessments == null || assessments.isEmpty()) {
+            return 0.0;
+        }
+        double totalEarned = 0.0;
+        double totalMaxScore = 0.0;
+        for (Assessment a : assessments) {
+            if (a.isGraded()) {
+                totalEarned += a.getScore();
+                totalMaxScore += a.getMaxScore();
+            }
+        }
+        if (totalMaxScore == 0) {
+            return 0.0;
+        }
+        return (totalEarned / totalMaxScore) * 100;
     }
 
     @Override
     public String getBreakdown(List<Assessment> assessments) {
-        // TODO
-        return "";
+        StringBuilder sb = new StringBuilder();
+        double totalEarned = 0.0;
+        double totalMaxScore = 0.0;
+        if (assessments != null) {
+            for (Assessment a : assessments) {
+                if (a.isGraded()) {
+                    sb.append(String.format("%s: %.1f / %.1f%n", a.getTitle(), a.getScore(), a.getMaxScore()));
+                    totalEarned += a.getScore();
+                    totalMaxScore += a.getMaxScore();
+                }
+            }
+        }
+        sb.append("---").append(System.lineSeparator());
+        double pct = totalMaxScore == 0 ? 0.0 : (totalEarned / totalMaxScore) * 100;
+        sb.append(String.format("Total: %.1f / %.1f (%.2f%%)%n", totalEarned, totalMaxScore, pct));
+        sb.append(String.format("Course total possible: %.1f", totalPossible));
+        return sb.toString();
     }
 
     @Override
     public double projectNeeded(List<Assessment> assessments, double targetGrade) {
-        // TODO
-        return 0.0;
+        if (assessments == null || assessments.isEmpty()) {
+            return -1.0;
+        }
+        if (totalPossible <= 0) {
+            return -1.0;
+        }
+        double earnedSoFar = 0.0;
+        double maxScoreSoFar = 0.0;
+        for (Assessment a : assessments) {
+            if (a.isGraded()) {
+                earnedSoFar += a.getScore();
+                maxScoreSoFar += a.getMaxScore();
+            }
+        }
+        double remainingMaxScore = totalPossible - maxScoreSoFar;
+        double requiredPoints = (targetGrade / 100.0) * totalPossible - earnedSoFar;
+
+        if (remainingMaxScore <= 0) {
+            return requiredPoints <= 0 ? 0.0 : -1.0;
+        }
+
+        double requiredPercentage = (requiredPoints / remainingMaxScore) * 100;
+        if (requiredPercentage > 100) {
+            return -1.0;
+        }
+        if (requiredPercentage < 0) {
+            return 0.0;
+        }
+        return requiredPercentage;
     }
 }
